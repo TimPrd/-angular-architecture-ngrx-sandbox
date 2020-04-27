b656e35714c64b582e4f77128d9c331d9526b773
TimPrd <timothe.pardieu@outlook.com>
2020-04-27 09:18:35 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,6 @@ import { PageNotFoundComponent } from "./shared/ui/page-not-found/page-not-found
 import { SelectivePreloadingStrategyService } from "./core/services/selective-preloading-strategy.service";
 
 const appRoutes: Routes = [
-  // {
-  //   path: "admin",
-  //   loadChildren: () => import("./admin/admin.module").then(m => m.AdminModule),
-  //   //canLoad: [AuthGuard]
-  // },
   {
     path: "",
     redirectTo: "/todos",
